a32ffb98d4e45cf0e62349d9ed8a938526bda597
Ingo Fischer <github@fischer-ka.de>
2022-11-18 20:50:58 +0100

    enhance logic, increase version

--- index.js
+++ index.js
@@ -4,6 +4,33 @@ const { JsonlDB } = require('@alcalzone/jsonl-db');
 const fs = require('fs');
 const path = require('path');
 
+function queryOwnIps() {
+    const ownIpArr = [];
+    try {
+        const ifaces = require('os').networkInterfaces();
+        Object.keys(ifaces).forEach(dev =>
+            ifaces[dev].forEach(
+                details =>
+                    // noinspection JSUnresolvedVariable
+                    !details.internal && ownIpArr.push(details.address)
+            )
+        );
+    } catch (e) {
+        console.error(`Can not query local IPs: ${e.message}`);
+    }
+    return ownIpArr;
+}
+
+function isLocalDbServer(host, ownIpArr) {
+    if (typeof host !== 'string') { // Host is invalid or a sentinel array with redis server list
+        return false;
+    }
+    return host === 'localhost' || host === '127.0.0.1' || // reachable locally only, seems single host system
+        host === '0.0.0.0' ||  // reachable by all others, seems a master host
+        !ownIpArr.length || // we were not able to find any IP, so we assume it is a single host system
+        ownIpArr.includes(host); // host is in the own IP list
+}
+
 async function compressDB(dbPath) {
     const db = new JsonlDB(dbPath);
     await db.open();
@@ -17,28 +44,57 @@ async function main() {
         dbPath = process.cwd();
         console.log(`No path given, using ${dbPath}`);
     }
-    const statesFile = path.join(dbPath, 'states.jsonl');
+
+    const configFile = path.join(dbPath, 'iobroker.json');
+    let config;
     try {
-        if (fs.existsSync(statesFile)) {
-            console.log(`Compressing ${statesFile}`);
-            await compressDB(statesFile);
-        } else {
-            console.log('states.jsonl not found to compress, skip');
+        if (!fs.existsSync(configFile)) {
+            console.log(`${dbPath} is not a valid ioBroker directory, skip`);
+            return;
         }
+        config = JSON.parse(fs.readFileSync(configFile, 'utf8'));
     } catch (e) {
-        console.log(`Cannot compress states.jsonl: ${e.stack}`);
+        console.log(`Cannot check config file in ${dbPath}: ${e.stack}`);
     }
-    const objectsFile = path.join(dbPath, 'objects.jsonl');
-    try {
-        if (fs.existsSync(objectsFile)) {
-            console.log(`Compressing ${objectsFile}`);
-            await compressDB(objectsFile);
-        } else {
-            console.log('objects.jsonl not found to compress, skip');
+
+    const ownIps = queryOwnIps();
+    let compressCount = 0;
+
+    if (config && config.states && config.states.type !== 'redis' && isLocalDbServer(config.states.host, ownIps)) {
+        const statesFile = path.join(dbPath, 'states.jsonl');
+        try {
+            if (fs.existsSync(statesFile)) {
+                console.log(`Compressing ${statesFile}`);
+                await compressDB(statesFile);
+                compressCount++;
+            } else {
+                console.log('states.jsonl not found to compress, skip');
+            }
+        } catch (e) {
+            console.log(`Cannot compress states.jsonl: ${e.stack}`);
         }
-    } catch (e) {
-        console.log(`Cannot compress objects.jsonl: ${e.stack}`);
+    }
+
+    if (config && config.objects && config.objects.type !== 'redis' && isLocalDbServer(config.objects.host, ownIps)) {
+        const objectsFile = path.join(dbPath, 'objects.jsonl');
+        try {
+            if (fs.existsSync(objectsFile)) {
+                console.log(`Compressing ${objectsFile}`);
+                await compressDB(objectsFile);
+                compressCount++;
+            } else {
+                console.log('objects.jsonl not found to compress, skip');
+            }
+        } catch (e) {
+            console.log(`Cannot compress objects.jsonl: ${e.stack}`);
+        }
+    }
+
+    if (compressCount === 0) {
+        console.log('No JSONL files found to compress, skip');
+    } else {
+        console.log(`Compressed ${compressCount} JSONL files. Done`);
     }
 }
 
-main().then(() => process.exit(0)).catch(e => {});
+main().then(() => process.exit(0)).catch(_e => {});
